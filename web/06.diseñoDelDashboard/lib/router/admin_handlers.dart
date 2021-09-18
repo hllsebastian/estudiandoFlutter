d8ffc96e15762e6b53ec34e289ac39bc946e4a9f
hllsebas <hllsebastian@gmail.com>
2021-09-18 01:13:51 -0500

    Inicando Seccion 9

--- web/06.diseñoDelDashboard/lib/router/admin_handlers.dart
+++ web/06.diseñoDelDashboard/lib/router/admin_handlers.dart
@@ -6,18 +6,10 @@ import 'package:admin_dashboard/ui/views/login_view.dart';
 import 'package:admin_dashboard/ui/views/register_view.dart';
 import 'package:provider/provider.dart';
 
-// Aca se definio el controlador del dashboard
-
 class AdminHandlers {
 
   static Handler login = Handler(
     handlerFunc: ( context, params ) {
-
-      // PARA LOGIN
-
-      // Si el estado de autenticacion es "AuthStatus.notAuthenticated",
-      // entonces regresara la vista de login, de lo contrario regresara
-      // la vista AuthStatus.notAuthenticated
 
       final authProvider = Provider.of<AuthProvider>(context!);
 
@@ -32,12 +24,6 @@ class AdminHandlers {
   static Handler register = Handler(
     handlerFunc: ( context, params ) {
       
-      // PARA REGISTRO
-
-      // Si el estado de autenticacion es "AuthStatus.notAuthenticated",
-      // entonces regresara la vista de registro, de lo contrario regresara
-      // la vista del DashboardView
-
       final authProvider = Provider.of<AuthProvider>(context!);
       
       if ( authProvider.authStatus == AuthStatus.notAuthenticated )
